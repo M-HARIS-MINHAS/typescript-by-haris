b190b30d8dedc85692c49fb6bc63336f1f52f3be
M-HARIS-MINHAS <minhasharis81@gmail.com>
2024-05-27 02:02:52 +0500

    Add files via upload

---
+++ step 00a hello world/main.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let message = "Hello world"; //firstly make a variable.
+//Infering Type,inferred as string.
+//take your cursor on the variable name
+console.log(message);
--- step 00a hello world/main.ts
+++ step 00a hello world/main.ts
@@ -1,1 +1,5 @@
+let message ="Hello world"//firstly make a variable.
+                           //Infering Type,inferred as string.
+                           //take your cursor on the variable name
 
+console.log(message);
