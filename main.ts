ddc1edfe21e42f2079782e0796cda1270ce03a73
M-HARIS-MINHAS <minhasharis81@gmail.com>
2024-08-28 13:27:33 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,9 @@
+//strongly type syntax
+let name:string ="haris";
+let age:number =17;
+let isAdmission:boolean =true;
+
+//type inference
+let name1 ="hashir";
+let age1 =19;
+let isAdmission1 =false;
