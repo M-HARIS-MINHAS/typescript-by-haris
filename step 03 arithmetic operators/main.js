d251c2652e5f18dd261165da85a02df949cb3ee3
M-HARIS-MINHAS <minhasharis81@gmail.com>
2024-08-28 13:50:09 +0500

    Add files via upload

---
+++ step 03 arithmetic operators/main.js
@@ -0,0 +1,17 @@
+"use strict";
+//arithmetic operators
+let a = 8;
+let b = 2;
+let c = 6;
+console.log(a + b); //10
+console.log(a - b); //6
+console.log(a * b); //16
+console.log(a / b); //4
+console.log(a % b); //0
+console.log(a ** 2); //64*/
+// post -increment 
+//console.log(a++)//8
+//console.log(a++)//9
+// pre -increment 
+console.log(++b); //3
+console.log(++b); //4
--- step 03 arithmetic operators/main.ts
+++ step 03 arithmetic operators/main.ts
@@ -1,1 +1,19 @@
+//arithmetic operators
+let a:number = 8;
+let b:number = 2;
+let c:number =6
+console.log(a+b)//10
+console.log(a-b)//6
+console.log(a*b)//16
+console.log(a/b)//4
+console.log(a%b)//0
+console.log(a**2)//64*/
 
+
+// post -increment 
+console.log(a++)//8
+console.log(a++)//9
+
+// pre -increment 
+console.log(++b)//3
+console.log(++b)//4
