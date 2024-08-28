3198369c1ea1a35cef0208d9b07f2c7dbf535fa4
M-HARIS-MINHAS <minhasharis81@gmail.com>
2024-08-28 13:52:58 +0500

    Add files via upload

---
+++ step 03-b assignment operators/main.js
@@ -0,0 +1,17 @@
+"use strict";
+//assignment operators
+let a = 5; //5 is value is assign to value a
+//compund assignment operators
+a += 5; //Equilvaent to a =a+5
+console.log(a); //output is 10
+a -= 5; //Equilvaent to a =a-5
+console.log(a); //,output is 5
+a *= 5; //Equilvaent to a =a*5
+console.log(a); //,output is 25
+a /= 5; //Equilvaent to a =a/5
+console.log(a); //,output is 5
+a %= 4; //Equilvaent to a =a%5
+console.log(a); //,output is 1
+let b = 6;
+b **= 5; //Equilvaent to a =b**5
+console.log(b); //,output is 25
