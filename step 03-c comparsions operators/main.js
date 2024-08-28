d5049d8d9d3bba801063d05d44b02c6ef68e5ca6
M-HARIS-MINHAS <minhasharis81@gmail.com>
2024-08-28 14:46:38 +0500

    Add files via upload

---
+++ step 03-c comparsions operators/main.js
@@ -0,0 +1,28 @@
+"use strict";
+let a = 5;
+let b = 6;
+let c = 8;
+let d = "6";
+//comparsion operators
+//EQUAL TO
+console.log(a == 5); //true
+console.log(a == 6); //false
+//EQUAL VALUE AND EQUAL TYPE
+console.log(a === 5); //true
+console.log(a === 6); //false
+//NOT EQUAL
+console.log(a != 5); //false
+console.log(a != 6); //TRUE
+//NOT EQUAL VALUE AND NOT EQUAL TYPE
+console.log(a !== 5); //false
+console.log(a !== 6); //TRUE
+//GREATER THAN
+console.log(a > 4); //TRUE
+//LESS THAN
+console.log(a < 4); //false
+//GREATER THAN OR EQUAL TO
+console.log(a >= 4); //TRUE
+console.log(a >= 5); //TRUE
+//LESS THAN
+console.log(a <= 4); //false
+console.log(a <= 5); //TRUE
--- step 03-c comparsions operators/main.ts
+++ step 03-c comparsions operators/main.ts
@@ -1,1 +1,32 @@
+let a=5
 
+//comparsion operators
+//EQUAL TO
+console.log(a == 5)//true
+console.log(a == 6)//false
+
+//EQUAL VALUE AND EQUAL TYPE
+console.log(a === 5)//true
+console.log(a === 6)//false
+
+//NOT EQUAL
+console.log(a != 5)//false
+console.log(a != 6)//TRUE
+
+//NOT EQUAL VALUE AND NOT EQUAL TYPE
+console.log(a !== 5)//false
+console.log(a !== 6)//TRUE
+
+//GREATER THAN
+console.log(a > 4)//TRUE
+
+//LESS THAN
+console.log(a < 4)//false
+
+//GREATER THAN OR EQUAL TO
+console.log(a >= 4)//TRUE
+console.log(a >= 5)//TRUE
+
+//LESS THAN
+console.log(a <= 4)//false
+console.log(a <= 5)//TRUE
