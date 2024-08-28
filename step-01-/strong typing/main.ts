20ce129b78ad84072f0f5fd6ee5ed6775119ab2a
M-HARIS-MINHAS <minhasharis81@gmail.com>
2024-08-28 13:50:37 +0500

    Delete step-01-/strong typing directory

--- step-01-/strong typing/main.ts
+++
@@ -1,9 +0,0 @@
-//strongly type syntax
-let name:string ="haris";
-let age:number =17;
-let isAdmission:boolean =true;
-
-//type inference
-let name1 ="hashir";
-let age1 =19;
-let isAdmission1 =false;
