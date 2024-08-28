5614731bd70494a8132feaad1fe71ceb29b0e7a1
M-HARIS-MINHAS <minhasharis81@gmail.com>
2024-08-28 13:45:52 +0500

    Add files via upload

---
+++ step 02 let-const/main.js
@@ -0,0 +1,22 @@
+"use strict";
+/*Const:variable cannot be re-declared or updated.A block scope variable.
+const name1 ="haris";
+name1 ="hashir"//give cursuor to name 1,name 1 cannot be updated
+
+const name1 ="hasham";//give cursuor to name 1,name 1 cannot be re-declared
+
+
+
+//Let:variable cannot be re-declared but can be updated.A block scope variable
+let name2 ="usman";
+name2 ="umar"//variable can be updated in let
+
+let name2 ="usman";//give cursuor to name 2,variable cannot be re-declared*/
+Object.defineProperty(exports, "__esModule", { value: true });
+if (true) {
+    let a = "ali";
+    console.log(a);
+}
+else {
+}
+//console.log(a)//it can not run because let is block-scope variable
--- step 02 let-const/main.ts
+++ step 02 let-const/main.ts
@@ -1,1 +1,24 @@
+//Const:variable cannot be re-declared or updated.A block scope variable.
+const name1 ="haris";
+name1 ="hashir"//give cursuor to name 1,name 1 cannot be updated
+
+const name1 ="hasham";//give cursuor to name 1,name 1 cannot be re-declared
+
+
+
+//Let:variable cannot be re-declared but can be updated.A block scope variable
+let name2 ="usman";
+name2 ="umar"//variable can be updated in let
+
+let name2 ="usman";//give cursuor to name 2,variable cannot be re-declared
+
+
+if(true){
+    let a="ali";
+    console.log(a)
+}
+else{
+   console.log("not working") //this block scope is not work
+}
+console.log(a)//it can not run because let is block-scope variable
 
